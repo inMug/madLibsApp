4dc85924431d95b60ad719e386588266643d82af
inMug <98911001+inMug@users.noreply.github.com>
2022-03-20 16:08:15 +0200

    Added mad-lib replacement variables to story

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
-let originalStory = ["A Wise Old Owl
-There was an old owl that lived in an oak. 
+"use strict";
+
+let originalStory = `A Wise Old Owl
+There was an old ${userInputs[0]} that lived in an ${userInputs[1]}. 
 Everyday he saw incidents happening around him. 
-Yesterday he saw a boy helping an old man to carry a heavy basket. 
-Today he saw a girl shouting at her mother. 
-The more he saw the less he spoke.
-As he spoke less, he heard more. 
-He heard people talking and telling stories. 
-He heard a woman saying that an elephant jumped over a fence. 
-He also heard a man saying that he had never made a mistake.
+Yesterday he saw a ${userInputs[2]} helping an old man to carry a heavy basket. 
+Today he saw a ${userInputs[3]} shouting at her mother. 
+The more he saw the less he ${userInputs[4]}.
+As he spoke less, he heard ${userInputs[5]}. 
+He heard people ${userInputs[6]} and telling stories. 
+He heard a woman saying that an ${userInputs[7]} jumped over a fence. 
+He also heard a man saying that he had never made a ${userInputs[8]}.
 The old owl had seen and heard about what happened to people. 
-Some became better and some became worse. 
-But the old owl had become wiser each and every day."];
+Some became better and some became ${userInputs[9]}. 
+But the old owl had become ${userInputs[10]} each and every day.`;
+
+let userInputs = [];
+
+console.log(originalStory);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let originalStory = `A Wise Old Owl
+There was an old owl ${userInputs[0]} that lived in an oak ${userInputs[1]}. 
+Everyday he saw incidents happening around him. 
+Yesterday he saw a boy ${userInputs[2]} helping an old man to carry a heavy basket. 
+Today he saw a girl shouting at her mother ${userInputs[4]}. 
+The more he saw the less he spoke.
+As he spoke less, he heard more ${userInputs[5]}. 
+He heard people talking ${userInputs[6]} and telling stories. 
+He heard a woman saying that an elephant ${userInputs[7]} jumped over a fence. 
+He also heard a man saying that he had never made a mistake ${userInputs[8]}.
+The old owl had seen and heard about what happened to people. 
+Some became better and some became worse ${userInputs[9]}. 
+But the old owl had become wiser ${userInputs[10]} each and every day.`;*/
