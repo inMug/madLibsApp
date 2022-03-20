b9a1acb0a9553271a9f5ef6ff2f0bfc000e1e085
inMug <98911001+inMug@users.noreply.github.com>
2022-03-20 18:09:49 +0200

    Added userPrompt

--- app.js
+++ app.js
@@ -14,10 +14,30 @@ The old owl had seen and heard about what happened to people.
 Some became better and some became ${userInputs[9]}. 
 But the old owl had become ${userInputs[10]} each and every day.`;
 
-let userInputs = [];
+//let userInputs = [];
 
 //Total questions
 let numberOfQuestions = 11;
+
+//User prompts
+let userPrompt = "Enter a";
+let questionArray = [
+    `${userPrompt} bird type`,
+    `${userPrompt} tree type`,
+    `${userPrompt} person`,
+    `${userPrompt} person`,
+    `${userPrompt} parent`,
+    `${userPrompt} n adverb`,
+    `${userPrompt} verb`,
+    `${userPrompt} n animal`,
+    `${userPrompt} noun`,
+    `${userPrompt} n adverb`,
+    `${userPrompt} n adverb`,
+];
+let userInputs = [];
+for(let i = numberOfQuestions; i > 0; i-- ) {
+    console.log(i);
+}
 
 console.log(originalStory);
 
@@ -38,7 +58,7 @@ console.log(originalStory);
 There was an old owl ${userInputs[0]} that lived in an oak ${userInputs[1]}. 
 Everyday he saw incidents happening around him. 
 Yesterday he saw a boy ${userInputs[2]} helping an old man to carry a heavy basket. 
-Today he saw a girl shouting at her mother ${userInputs[4]}. 
+Today he saw a girl ${userInputs[3]} shouting at her mother ${userInputs[4]}. 
 The more he saw the less he spoke.
 As he spoke less, he heard more ${userInputs[5]}. 
 He heard people talking ${userInputs[6]} and telling stories. 
