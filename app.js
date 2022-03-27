9e9cf9af0a4b42de19bad2d59d47d58734d90533
inMug <98911001+inMug@users.noreply.github.com>
2022-03-27 14:33:10 +0300

    Eddited story withcorrect userInput array

--- app.js
+++ app.js
@@ -1,43 +1,25 @@
 "use strict";
 
-let originalStory = `A Wise Old Owl
-There was an old ${userInputs[0]} that lived in an ${userInputs[1]}. 
-Everyday he saw incidents happening around him. 
-Yesterday he saw a ${userInputs[2]} helping an old man to carry a heavy basket. 
-Today he saw a ${userInputs[3]} shouting at her mother. 
-The more he saw the less he ${userInputs[4]}.
-As he spoke less, he heard ${userInputs[5]}. 
-He heard people ${userInputs[6]} and telling stories. 
-He heard a woman saying that an ${userInputs[7]} jumped over a fence. 
-He also heard a man saying that he had never made a ${userInputs[8]}.
-The old owl had seen and heard about what happened to people. 
-Some became better and some became ${userInputs[9]}. 
-But the old owl had become ${userInputs[10]} each and every day.`;
-
-//let userInputs = [];
-
-//Total questions
-let numberOfQuestions = 11;
-
-//User prompts
-let userPrompt = "Enter a";
-let questionArray = [
-    `${userPrompt} bird type`,
-    `${userPrompt} tree type`,
-    `${userPrompt} person`,
-    `${userPrompt} person`,
-    `${userPrompt} parent`,
-    `${userPrompt} n adverb`,
-    `${userPrompt} verb`,
-    `${userPrompt} n animal`,
-    `${userPrompt} noun`,
-    `${userPrompt} n adverb`,
-    `${userPrompt} n adverb`,
+//user inputs (should go first and then story)
+let userInput = ["owl", "oak", "boy", "basket", "girl", "less", "more", "elephant", "mistake", "better", "wiser",
 ];
-let userInputs = [];
-for(let i = numberOfQuestions; i > 0; i-- ) {
-    console.log(i);
-}
+
+//original story with user inputs
+let originalStory = `A Wise Old ${userInput[0]}
+There was an ${userInput[0]} that lived in an ${userInput[1]}.
+Everyday he saw incidents happening around him.
+Yesterday he saw a ${userInput[2]} helping an old man to carry a heavy ${userInput[3]}.
+Today he saw ${userInput[4]} shouting at her mother. The more he saw the ${userInput[5]} he spoke.
+As he spoke less, he heard ${userInput[6]}. 
+He heard people talking and telling stories.
+He heard a woman saying that an ${userInput[7]} jumped over a fence.
+He also heard a man saying that he had never made a ${userInput[8]}.
+The old ${userInput[0]} had seen and heard about what happened to people.
+Some became ${userInput[9]} and some became worse.
+But the old ${userInput[0]} had become ${userInput[10]} each and every day.
+Moral of the story:
+You should be observant, talk less but listen more.
+This will make you a wise person.`;
 
 console.log(originalStory);
 
@@ -53,6 +35,109 @@ console.log(originalStory);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const originalStory = `A Wise Old Owl
+
+There was an old owl that lived in an oak. Everyday he saw incidents happening around him. Yesterday he saw a boy helping an old man to carry a heavy basket. Today he saw a girl shouting at her mother. The more he saw the less he spoke.
+
+
+As he spoke less, he heard more. He heard people talking and telling stories. He heard a woman saying that an elephant jumped over a fence. He also heard a man saying that he had never made a mistake.
+
+
+The old owl had seen and heard about what happened to people. Some became better and some became worse. But the old owl had become wiser each and every day.
+
+
+Moral of the story :
+
+
+You should be observant, talk less but listen more. This will make you a wise person.`;*/
+
+
+
+
+
+
+
+
+
+/*let originalStory = `A Wise Old Owl
+There was an old ${userInputs[0]} that lived in an ${userInputs[1]}. 
+Everyday he saw incidents happening around him. 
+Yesterday he saw a ${userInputs[2]} helping an old man to carry a heavy basket. 
+Today he saw a ${userInputs[3]} shouting at her mother. 
+The more he saw the less he ${userInputs[4]}.
+As he spoke less, he heard ${userInputs[5]}. 
+He heard people ${userInputs[6]} and telling stories. 
+He heard a woman saying that an ${userInputs[7]} jumped over a fence. 
+He also heard a man saying that he had never made a ${userInputs[8]}.
+The old owl had seen and heard about what happened to people. 
+Some became better and some became ${userInputs[9]}. 
+But the old owl had become ${userInputs[10]} each and every day.`;
+
+//Total questions
+let numberOfQuestions = 11;
+
+//User prompts
+let userPrompt = "Enter";
+let questionArray = [
+  `${userPrompt} a bird type`,
+  `${userPrompt} a tree type`,
+  `${userPrompt} a person`,
+  `${userPrompt} a person`,
+  `${userPrompt} a parent`,
+  `${userPrompt} an adverb`,
+  `${userPrompt} averb`,
+  `${userPrompt} an animal`,
+  `${userPrompt} a noun`,
+  `${userPrompt} an adverb`,
+  `${userPrompt} an adverb`,
+];
+
+let userInputs = [];
+let questionCounter = 0;
+for (let i = numberOfQuestions; i > 0; i--) {
+  console.log(i);
+  questionCounter++;
+  console.log(questionArray[questionCounter]);
+}
+
+//console.log(originalStory);
+
+/*let originalStory = `A Wise Old Owl
+There was an old ${userInputs[0]} that lived in an ${userInputs[1]}. 
+Everyday he saw incidents happening around him. 
+Yesterday he saw a ${userInputs[2]} helping an old man to carry a heavy basket. 
+Today he saw a ${userInputs[3]} shouting at her mother. 
+The more he saw the less he ${userInputs[4]}.
+As he spoke less, he heard ${userInputs[5]}. 
+He heard people ${userInputs[6]} and telling stories. 
+He heard a woman saying that an ${userInputs[7]} jumped over a fence. 
+He also heard a man saying that he had never made a ${userInputs[8]}.
+The old owl had seen and heard about what happened to people. 
+Some became better and some became ${userInputs[9]}. 
+But the old owl had become ${userInputs[10]} each and every day.`;*/
 
 /*let originalStory = `A Wise Old Owl
 There was an old owl ${userInputs[0]} that lived in an oak ${userInputs[1]}. 
